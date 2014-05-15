9bf43cd6805b335dac8503a666bc6c5062add9d8
HuangYu1 <huangyu1@staff.sina.com.cn>
2014-05-15 14:25:06 +0800

    add android

--- src/test/Helloworld.java
+++ src/test/Helloworld.java
@@ -5,5 +5,6 @@ public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("sina weibo");
+		System.out.println("android");
 	}
 }
